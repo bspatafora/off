3896a427cf1c2f30851d4a0afe21951ad7ba133e
bspatafora <benjamin.spatafora@gmail.com>
2015-09-01 09:12:24 -0500

    Start an API

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 ruby '2.2.2'
 
 gem 'rspec', '~> 3.3'
+gem 'sinatra', '~> 1.4.6'
---
+++ api/days_off.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+require_relative '../lib/day_off/presenter'
+require_relative '../spec/day_off/factory'
+
+get '/v1/days_off/:id' do
+  content_type :json
+
+  DayOff::Presenter.do(DayOff::Factory.make)
+end
